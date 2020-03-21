1abaa945442c7ddf14676139d04a29667aacda95
Michaela DeCamara <michaela.decamara@gmail.com>
2020-03-21 18:50:20 -0400

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from "./contacts.json"
 
 class App extends Component {
+  state = {
+    contacts: data.splice(0, 5)
+  }; 
+
+  addRandomActor = () => {
+    const randomNo = ~~(Math.random() * data.length);
+    let randomContact = data.splice(randomNo, 1);
+    this.setState({
+      contacts: [...this.state.contacts, randomContact[0]]
+    });
+
+
+  sortByName = () => {
+    let copyOfTheContacts = [...this.state.contacts];
+    copyOfTheContacts.sort((s,b) => {
+      return a.name > b.name ? 1: -1;
+    });
+  };
+
+  this.setState({
+    contacts: copyOfTheContacts
+  });
+
+
+  deleteContact = i => {
+     this.setState((prevState) => {
+       contacts: prevState.contacts.filter(el => el.id !== i.id);
+     });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <h1>IronContacts</h1>
+      <input></input>
+      <button onClic={this.addRandomActor}>Add Random Contact</
+button>
+<button onClic={this.sortByName}>Sort by name</
+button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.contact.map(actor => {
+            return(
+              <tr key={actor.id}>
+
+            )
+          })}
+        </tbody>
+      </table>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
